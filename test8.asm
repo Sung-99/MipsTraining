6915b6d6bd19cbae1b5c9496f71acfbbadd83044
Sung-99 <vitorhugomota99@hotmail.com>
2024-11-16 10:57:22 -0300

    up

--- test8.asm
+++ test8.asm
@@ -1,7 +1,8 @@
 .data
 Menu: .asciiz "==================MENU==================\n*Opção questão A(Dig 1)*\n*Opção questão B(Dig 2)*\n*Opção questão C(Dig 3)*\n*Opção questão D(Dig 4)*\n"
 Parametro1MSG: .asciiz "Digite o primeiro numero: "
-Parametro2MSG: .asciiz "Digite o primeiro numero: "
+Parametro2MSG: .asciiz "Digite o segundo numero: "
+Parametro3MSG: .asciiz "Digite o terceiro numero: "
 DigiteSuaEscolha: .asciiz "Digite um numero que corresponda ao menu: "
 .text
 la $a0, Menu
@@ -20,11 +21,50 @@ beq $t0, 4, Op4Procedimento   # Se ele digita 4, ira para uma label que vai enca
 li $v0, 10
 syscall #Alguma coisa alem das opções >>>>>>>>>>> diréto pro final
 
-Op1Procedimento: 
+# ===================== QUESTAO A ================================================
+Op1Procedimento:
+# ===================== PARAMETROS PARA A OPÇÃO 1 (LEVA COMO PARAMETRO (a1 e a2) PARA DENTRO DO JAL(FUNÇÃO))================================================	
+	la $a0, Parametro1MSG       #Carrego a msg primeiro parametro
+	li $v0, 4 		 #Direciono o sistema para exibir
+	syscall			 
+	li $v0, 5		#Lendo primeira entrada de parametro
+	syscall
+	move $a1, $v0		 #movo ela para a1
+	la $a0, Parametro2MSG	 #Carrego a msg segundo parametro
+	li $v0, 4		 #Direciono o sistema para exibir
+	syscall
+	li $v0, 5		#Lendo primeira entrada de parametro
+	syscall
+	move $a2, $v0		#movo ela para a2
+	#CHAMAR FUNÇÃO OP1(MAIOR OU MENOR NUMERO)
 	jal Op1
+	#retorno da função
 	j finish
-Op2Procedimento: 
+# ===================== QUESTAO B ================================================
+Op2Procedimento:
+# ===================== PARAMETROS PARA A OPÇÃO 2 (LEVA COMO PARAMETRO (a1, a2 e a3 ) PARA DENTRO DO JAL(FUNÇÃO))================================================	
+
+	la $a0, Parametro1MSG       #Carrego a msg primeiro parametro
+	li $v0, 4 		 #Direciono o sistema para exibir
+	syscall			 
+	li $v0, 5		#Lendo primeira entrada de parametro
+	syscall
+	move $a1, $v0		 #movo ela para a1
+	la $a0, Parametro2MSG	 #Carrego a msg segundo parametro
+	li $v0, 4		 #Direciono o sistema para exibir
+	syscall
+	li $v0, 5		#Lendo primeira entrada de parametro
+	syscall
+	move $a2, $v0		#movo ela para a2
+	la $a0, Parametro3MSG	 #Carrego a msg segundo parametro
+	li $v0, 4		 #Direciono o sistema para exibir
+	syscall
+	li $v0, 5		#Lendo primeira entrada de parametro
+	syscall
+	move $a3, $v0		#movo ela para a2
+	#CHAMAR FUNÇÃO OP2(ORDENAÇÃO)
 	jal Op2
+	#retorno da função
 	j finish
 Op3Procedimento: 
 	jal Op3
@@ -36,30 +76,18 @@ Op4Procedimento:
 
 # ===================== TUDO SOBRE A OPÇÃO 1 ================================================
 Op1: # --------------------- Opção 1 --------------------------
-	la $a0, Parametro1MSG       #Carrego a msg primeiro parametro
-	li $v0, 4 		 #Direciono o sistema para exibir
-	syscall			 
-	li $v0, 5		#Lendo primeira entrada de parametro
-	syscall
-	move $t1, $v0		 #movo ela para a0
-	la $a0, Parametro2MSG	 #Carrego a msg segundo parametro
-	li $v0, 4		 #Direciono o sistema para exibir
-	syscall
-	li $v0, 5		#Lendo primeira entrada de parametro
-	syscall
-	move $t2, $v0		#movo ela para a0
-	slt $t3, $t1, $t2	#confiro qual é o maior numero
+	slt $t3, $a1, $a2	#confiro qual é o maior numero
 	beq $t3, $zero, MaiorPrimeiro	#se for a0, mando para label referente ao a0 ser o maior
 	j MaiorSegundo
-MaiorPrimeiro:# Maior numero sedo o primeiro digitado logo na subtração será a0 - a1
-	sub $t4, $t1, $t2
+MaiorPrimeiro:# Maior numero sedo o primeiro digitado logo na subtração será a1 - a2
+	sub $t4, $a1, $a2
 	
-	slt $t0, $t1, $t4
-	beq $t0, $zero Retorna1 #Após o calculo da subtração preciso exibir 1 caso o primeiro digitado seja maior que a subtração de a0 - a1
-	j Retorna0 #Caso contrario, primeiro digitado seja menor que a0 - a1
-MaiorSegundo:	# Maior numero sedo o segundo digitado logo na subtração será a1 - a0
-	sub $t4, $t2, $t1
-	slt $t0, $t1, $t4
+	slt $t0, $a1, $t4
+	beq $t0, $zero Retorna1 #Após o calculo da subtração preciso exibir 1 caso o primeiro digitado seja maior que a subtração de a1 - a2
+	j Retorna0 #Caso contrario, primeiro digitado seja menor que a1 - a2
+MaiorSegundo:	# Maior numero sedo o segundo digitado logo na subtração será a2 - a1
+	sub $t4, $a2, $a1
+	slt $t0, $a1, $t4
 	beq $t0, $zero Retorna1 #Após o calculo da subtração preciso exibir 1 caso o primeiro digitado seja maior que a subtração de a1 - a0
 	j Retorna0 #Caso contrario, primeiro digitado seja menor que a1 - a0
 Retorna1:
@@ -77,8 +105,29 @@ Retorna0:
 
 # ===================== TUDO SOBRE A OPÇÃO 2 ================================================
 Op2:
-	addi $v0, $zero, 2
-	jr  $ra
+	slt $t0, $a2, $a1 #Confiro os 2 primeiros
+	beq $t0, 1, Ordena
+	jal OrdenaComplexa
+	slt $t0, $a1, $a2 #Confiro os 2 primeiros após a analise do "fluxo mais chatinho"
+	beq $t0, 1, finish
+Ordena: #após o retorno, meu a1 será menor que meu a2, entao preciso saber se meu menor numero é menor que o terceiro
+	move $t1, $a1
+	move $a1, $a2
+	move $a2, $t1
+	j Op2 #volto a fazer a conferencia indo para o fluxo inicial(obviamente vai passar do meu primeiro beq q confere os 2 primeiros numeros)
+	#resumidamente eu coloco meu menor numero (a2) em um auxiliar, depois coloco meu maior numero a1 em a2 e volto com meu auxiliar que está com meu menor numero para a1
+OrdenaComplexa:
+	      #essa ordenação mexe com o caso em que meu numero está em ultimo e o numero do meio ja é maior que o primeiro
+	      #por exemplo, ordenação simples : 5 1 3, eu simplesmente ando com o 5 até a ultima posição, isso segue o fluxo normal do código
+	      #	fluxo onde a2 < a1 ====*(após a troca)* a2 < a3 ===== fim
+	      #fluxo mais chatinho a1 < a2 > a3 ou a1 > a2 > a3 aonde meu primeiro beq será pulado 
+	      
+	slt $t0, $a3, $a2 #Confiro os 2 ultimos e se meu ultimo é maior que meu segundo numero, ele é maior que o primeiro tambem
+	beq $t0, 0, finish
+	move $t1, $a2
+	move $a2, $a3
+	move $a3, $t1
+	jr $ra #caso caia no fluxo mais chatinho, preciso conferir se meu segundo numero é menor que meu primeiro novamente
 # ===================== FIM DE TUDO SOBRE A OPÇÃO 32=========================================
 # ===================== TUDO SOBRE A OPÇÃO 3 ================================================
 Op3:
